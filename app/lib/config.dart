75034b95fcab8fdac5383186def525116e2b00c3
Aeneas Tews <aeneas.tews@tum.de>
2026-06-02 11:52:13 +0200

    changed default from https to http

--- app/lib/config.dart
+++ app/lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String apiBaseUrl = String.fromEnvironment(
     "API_BASE_URL",
-    defaultValue: "https://localhost:8000"
+    defaultValue: "http://localhost:8000"
   );
 }
